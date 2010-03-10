34f62548a49bf1d154939b6491287323f4830481
Amiel Martin <amiel@carnesmedia.com>
2010-03-10 12:30:25 -0800

    A few other misc changes from the template
    * action_mailer uses sendmail for development
    * s3 config scaffold for use with paperclip
    * setup PATH for macports to give paperclip access to identify and convert
    * first controller home
    * time zone: Pacific Time (US & Canada)
    * default locale
    * a couple of i18n strings that are used in application_helper
    * some basic sprockets setup
    * formtastic setup
    * spreadhead setup
    * filter parameter logging for authlogic
    * cmadmin files that need to be synced

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,17 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  include Spreadhead::Render
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password, :password_confirmation
+
+  layout :no_layout_for_xhr
+  private
+  def no_layout_for_xhr
+    request.xhr? ? nil : 'application'
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  SprocketsApplication.routes(map)
+
+  map.root :controller => 'home'
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:

f9c733361aec235cf7bdb0a0d8d883b0450bfe7d
Amiel Martin <amiel@carnesmedia.com>
2010-03-15 15:53:39 -0700

    current class

--- app/javascripts/new_submission.js
+++ app/javascripts/new_submission.js
@@ -3,17 +3,27 @@
 $(document).ready(function() {
 
 	function show_hide(to_show, to_hide) {
-		$(to_show).slideDown();
 		$(to_hide).slideUp();
-		return false;
+		$(to_show).stop().slideDown();
 	}
 		
-	$('.candybar').as_scope(function($) {	
-		$('.address').click(function(){ return show_hide('#location_address', '#location_latlng'); }).click();
-		$('.latlng').click(function(){  return show_hide('#location_latlng',  '#location_address'); });
+	$('.candybar').as_scope(function($) {
+	    
+	    function candybar(scope, me, others) {
+	        var others_ids = jQuery.map(others, function(t) { return '#' + scope + '_' + t; }).join(', '),
+	         others_classes = jQuery.map(others, function(t) { return '.' + t; }).join(', ');
+	        show_hide('#' + scope + '_' + me, others_ids);
+	        
+	        $(others_classes).removeClass('current');
+	        $('.' + me).addClass('current');
+	        return false;
+	    }
+	    	
+		$('.address').click(function(){ return candybar('location', 'address', ['latlng']); }).click();
+		$('.latlng').click(function(){  return candybar('location', 'latlng', ['address']); });
 		
-		$('.photo').click(function(){ return show_hide('#media_photo', '#media_video, #media_none'); }).click();
-		$('.video').click(function(){ return show_hide('#media_video', '#media_photo, #media_none'); });
-		$('.none').click(function(){  return show_hide('#media_none',  '#media_photo, #media_video'); });
+		$('.photo').click(function(){ return candybar('media', 'photo', ['video', 'none']); }).click();
+		$('.video').click(function(){ return candybar('media', 'video', ['photo', 'none']); });
+		$('.none').click(function(){  return candybar('media', 'none',  ['photo', 'video']); });
 	});
 });
